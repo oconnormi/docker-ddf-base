95dcfd3a3b499acecbc59888dd95c1f4042e1a51
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2017-03-17 09:23:25 -0400

    Fixed issue where default localhost user could be deleted even when user should be set to localhost

--- alpine/entrypoint/pre_start.sh
+++ alpine/entrypoint/pre_start.sh
@@ -12,8 +12,8 @@ $ENTRYPOINT_HOME/certs.sh
 
 props set org.codice.ddf.system.hostname $_app_hostname $APP_HOME/etc/system.properties
 props set org.codice.ddf.system.siteName $_app_hostname $APP_HOME/etc/system.properties
-props set $_app_hostname $_app_hostname,group,admin,manager,viewer,system-admin,system-history,systembundles $APP_HOME/etc/users.properties
 props del localhost $APP_HOME/etc/users.properties
+props set $_app_hostname $_app_hostname,group,admin,manager,viewer,system-admin,system-history,systembundles $APP_HOME/etc/users.properties
 sed -i "s/localhost/$_app_hostname/" $APP_HOME/etc/users.attributes
 
 if [ -n "$SOLR_ZK_HOSTS" ]; then
