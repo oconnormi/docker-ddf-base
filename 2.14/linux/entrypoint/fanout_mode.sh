78e76ba8083739b9cfc9f257a054e2d166feabd5
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2019-05-08 07:53:38 -0400

    Updated with log output and create config file when it doesn't exist

--- 2.14/linux/entrypoint/fanout_mode.sh
+++ 2.14/linux/entrypoint/fanout_mode.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 source ${ENTRYPOINT_HOME}/global_env.sh
 
-props set fanoutEnabled true ${_app_etc}/ddf.catalog.CatalogFrameworkImpl.config
+_catalog_framework_config=${_app_etc}/ddf.catalog.CatalogFrameworkImpl.config
+
+echo "'CATALOG_FANOUT_MODE is set to true. Enabling Catalog Fanout Mode now!"
+
+if [ ! -f "${_catalog_framework_config}" ]; then
+  touch ${_catalog_framework_config}
+fi
+
+props set fanoutEnabled true ${_catalog_framework_config}
