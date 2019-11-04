5610a1f8b0121641736137a631e779bf374dd4af
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2019-11-04 07:39:08 -0500

    Removed fixed credentials

--- entrypoint/disable_idp.sh
+++ entrypoint/disable_idp.sh
@@ -2,12 +2,20 @@
 
 source ${ENTRYPOINT_HOME}/global_env.sh
 
-curl -k -X POST \
-  "https://${_system_internal_hostname}:${_system_internal_https_port}/admin/jolokia/exec/org.codice.ddf.ui.admin.api.ConfigurationAdmin:service=ui,version=2.3.0/add" \
-  -H 'Authorization: Basic YWRtaW46YWRtaW4=' \
-  -H 'Content-Type: application/json' \
-  -H "Origin: ${_system_internal_hostname}:${_system_internal_https_port}" \
-  -H "Referer: https://${_system_internal_hostname}:${_system_internal_https_port}" \
-  -H 'X-Requested-With: Curl' \
-  -H 'cache-control: no-cache' \
-  -d @${ENTRYPOINT_HOME}/idp_off.json
+_user=$(grep ${_users_properties_file} admin= | cut -d '=' -f 1)
+_pass=$(grep ${_users_properties_file} admin= | cut -d '=' -f 2 | cut -d ',' -f 1 | )
+
+_auth=$(echo "${_user}:${_pass}" | base64)
+
+if [ ! -z "${_pass}" ]; then
+
+  curl --head --silent --output /dev/null -k -X POST \
+    "https://${_system_internal_hostname}:${_system_internal_https_port}/admin/jolokia/exec/org.codice.ddf.ui.admin.api.ConfigurationAdmin:service=ui,version=2.3.0/add" \
+    -H "Authorization: Basic ${_auth}" \
+    -H 'Content-Type: application/json' \
+    -H "Origin: ${_system_internal_hostname}:${_system_internal_https_port}" \
+    -H "Referer: https://${_system_internal_hostname}:${_system_internal_https_port}" \
+    -H 'X-Requested-With: Curl' \
+    -H 'cache-control: no-cache' \
+    -d @${ENTRYPOINT_HOME}/idp_off.json
+fi
