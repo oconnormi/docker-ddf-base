ba8ebcedb840c385d6442a6b0e3fa384ca6b482e
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2019-11-04 09:07:27 -0500

    Fixed disable IdP step

--- entrypoint/disable_idp.sh
+++ entrypoint/disable_idp.sh
@@ -2,14 +2,14 @@
 
 source ${ENTRYPOINT_HOME}/global_env.sh
 
-_user=$(grep ${_users_properties_file} admin= | cut -d '=' -f 1)
-_pass=$(grep ${_users_properties_file} admin= | cut -d '=' -f 2 | cut -d ',' -f 1 | )
+_user=admin
+_pass=$(grep "${_user} =" ${_users_properties_file} | cut -d '=' -f 2 | cut -d ',' -f 1 | awk '{$1=$1};1')
 
-_auth=$(echo "${_user}:${_pass}" | base64)
+_auth=$(echo -n "${_user}:${_pass}" | base64)
 
 if [ ! -z "${_pass}" ]; then
 
-  curl --head --silent --output /dev/null -k -X POST \
+  response=$(curl --silent --write-out %{http_code} --output /dev/null -k -X POST \
     "https://${_system_internal_hostname}:${_system_internal_https_port}/admin/jolokia/exec/org.codice.ddf.ui.admin.api.ConfigurationAdmin:service=ui,version=2.3.0/add" \
     -H "Authorization: Basic ${_auth}" \
     -H 'Content-Type: application/json' \
@@ -17,5 +17,6 @@ if [ ! -z "${_pass}" ]; then
     -H "Referer: https://${_system_internal_hostname}:${_system_internal_https_port}" \
     -H 'X-Requested-With: Curl' \
     -H 'cache-control: no-cache' \
-    -d @${ENTRYPOINT_HOME}/idp_off.json
+    -d @${ENTRYPOINT_HOME}/idp_off.json)
+  echo "IdP Disabled Status code: ${repsonse}"
 fi
